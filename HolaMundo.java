abd1b9fd5dedc34554b25b8a8239b36ebebec1a8
Nightdragoon <87211704+Nightdragoon@users.noreply.github.com>
2022-11-16 16:38:37 -0600

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String letras = "Nothing here to see\n" +
+"Just a kid like me\n" +
+"Trying to cut some teeth\n" +
+"Trying to figure it out\n" +
+"Nothing better to do\n" +
+"When I'm stuck on you\n" +
+"I'm still in here trying to figure it out\n" +
+"Getting hard to sleep\n" +
+"Blood is in my dreams\n" +
+"Love is killing me\n" +
+"Trying to figure it out\n" +
+"Nothing better to do\n" +
+"When I'm stuck on you\n" +
+"I'm still in here trying to figure it out\n" +
+"I let it go, 'cause I won't see you later\n" +
+"And we're not allowed to talk it out\n" +
+"I said I'd go, put myself on show\n" +
+"But I'm still trying to figure it out\n" +
+"I broke my shoe, tripped and fell on you\n" +
+"But you didn't know I planned it out\n" +
+"I said I'd go, yeah, I won't see you later\n" +
+"And we're not allowed to figure it out, oh\n" +
+"Nothing here to see\n" +
+"Just a kid like me\n" +
+"Trying to cut some teeth\n" +
+"Trying to figure it out\n" +
+"Nothing better to do\n" +
+"When I'm stuck on you\n" +
+"I'm still in here trying to figure it out\n" +
+"I let it go, 'cause I won't see you later\n" +
+"And we're not allowed to talk it out\n" +
+"I said I'd go, put myself on show\n" +
+"But I'm still trying to figure it out\n" +
+"I broke my shoe, tripped and fell on you\n" +
+"But you didn't know I planned it out\n" +
+"I said I'd go, yeah I won't see you later\n" +
+"And we're not allowed to figure it out";
+        // TODO code application logic here
+        System.out.println(letras);
+ 
+    }
+    
+}
